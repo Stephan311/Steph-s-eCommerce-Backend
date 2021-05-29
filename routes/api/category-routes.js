77031de2325491526e7c4016271dc3484375760d
stephan du Plooy <sdpphotography1@gmail.com>
2021-05-29 10:02:12 -0400

    Added DELETE for Categories by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,6 +54,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((deletedcategory) => {
+    res.json(deletedcategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
