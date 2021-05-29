e9d5f8ad1b4ab9b31a81bffd47386f29c105044f
stephan du Plooy <sdpphotography1@gmail.com>
2021-05-29 09:21:37 -0400

    Added GET by Id for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,6 +14,9 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categorydata) => {
+    res.json(categorydata);
+  })
 });
 
 
