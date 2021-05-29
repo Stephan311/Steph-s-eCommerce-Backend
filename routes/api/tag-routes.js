77bb5b2b3cee0277633a1e449385624ae88045ba
stephan du Plooy <sdpphotography1@gmail.com>
2021-05-29 09:48:40 -0400

    Added PUT by Id for Tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,6 +34,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }).then((updatetag) => {
+      res.json(updatetag);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
